72e3c4f3da623abc8d8f900a2dac57bc7c5a82a4
danistrigaro <daniele.strigaro@gmail.com>
2016-10-28 17:50:17 +0200

    ipdate workingpackages page

--- src/components/workingpackages.jsx
+++ src/components/workingpackages.jsx
@@ -1,18 +1,67 @@
 //react
 import React from 'react'
+// react search filter
+import SearchInput, {createFilter} from 'react-search-input'
 //material ui
+import {List, ListItem} from 'material-ui/List';
 import Divider from 'material-ui/Divider';
+//data
+import wpData from '../data/data.js'
+const KEYS_TO_FILTERS = ['name']
 
 const Workingpackages = React.createClass({
+  getInitialState () {
+    return { searchTerm: '' }
+  },
+  searchUpdated (term) {
+    this.setState({searchTerm: term})
+  },
   render () {
+    const filteredWpData = wpData.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
     return (
       <div id='page-top'>
         <div className='container'>
           <h2 className='title-page'>Working packages (WP)</h2>
           <Divider />
           <p className='paragraph'>
-
+            According to the presented research plan WP leaders are responsible for the coordination of
+            their WP’s tasks. Within the WPs participants are responsible of different individual tasks. Work
+            packages 2, 3 and 4 envision scientific and technical activities that will require intense and co-
+            ordinated work among the partners: for this reason scientific meetings will be organized by WP
+            leaders based on individuated needs. The role and collaboration of different disciplines and per-
+            sonnel can be generally summarized as follow:
           </p>
+          <SearchInput className="search-input" onChange={this.searchUpdated} />
+          <List>
+          {filteredWpData.map((wp, index) => (
+            <ListItem
+              key={'wp-key-'+index}
+              primaryText={`${wp.name} - ${wp.title}`}
+              secondaryText={<div style={{height:'100%'}}>
+                <p style={{marginTop:'10px'}}>Duration: <strong>{wp.duration}</strong></p>
+                <p>Participants: <strong>{wp.participants}</strong></p>
+                <p>Lead partner: <strong>{wp.leadpartner}</strong></p>
+              </div>}
+              nestedItems={[
+                    <ListItem
+                      key={'objective-key'}
+                      primaryText='Objectives'
+                      nestedItems={wp.objectives.map((obj, index2) => (
+                        <ListItem key={'objective-key-'+index2} secondaryText={(index2+1)+' - '+obj} />
+                      ))}
+                    />,
+                    <ListItem
+                      key={'deliverables-key'}
+                      primaryText='Deliverables'
+                      nestedItems={wp.deliverables.map((del, index3) => (
+                        <ListItem key={'deliverables-key-'+index3} secondaryText={`${del.code} - ${del.title} (${del.duration})`} />
+                      ))}
+                    />,
+                    
+                  ]}
+            />
+          ))}
+          </List>
         </div>
       </div>
     )
