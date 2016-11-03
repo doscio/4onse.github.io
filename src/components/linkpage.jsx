a8ec52b89d99948a69a10225484e5cbc330b30eb
danistrigaro <daniele.strigaro@gmail.com>
2016-11-03 11:23:12 +0100

    updated link page

--- src/components/linkpage.jsx
+++ src/components/linkpage.jsx
@@ -23,29 +23,41 @@ const LinkPage = React.createClass({
             <Table striped bordered condensed hover>
               <thead>
                 <tr>
-                  <th></th>
-                  <th></th>
-                  <th></th>
-                  <th></th>
+                  <th>Name</th>
+                  <th>Description</th>
+                  <th>URL</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
+                  <td><strong>BDROP</strong></td>
+                  <td>It provides access to the project data through a web interface, sync clients or WebDAV while providing a platform to view, sync and share across devices easily</td>
+                  <td><a target='_blank' href='https://b2drop.eudat.eu/f/2803196'>Click here!</a></td>
                 </tr>
                 <tr>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
+                  <td><strong>Calendar</strong></td>
+                  <td>When, where and what are the next meetings, actions, etc. </td>
+                  <td><a target='_blank' href='https://www.google.com/calendar/render?cid=NG9uc2Uuc3Vwc2lAZ21haWwuY29t&invEmailKey=daniele.strigaro@supsi.ch:944000be83fda147298c4754fa2ac1aa9e884b20'>Click here!</a></td>
                 </tr>
                 <tr>
-                  <td></td>
-                  <td colSpan="2"></td>
-                  <td></td>
+                  <td><strong>OSGEO</strong></td>
+                  <td>The Open Monitoring Systems Working Group is a subsidiary body established by the OSGeo Open Geoscience Committee to advance in the topic of Open monitoring system for in-situ observations</td>
+                  <td><a target='_blank' href='https://wiki.osgeo.org/wiki/Open_Monitoring_Systems_Working_Group'>Click here!</a></td>
+                </tr>
+                <tr>
+                  <td><strong>Github</strong></td>
+                  <td>GitHub is where we build software. GitHub provides an area to discover, fork, and contribute to the whole project's code developed.</td>
+                  <td><a target='_blank' href='https://github.com/4onse/'>Click here!</a></td>
+                </tr>
+                <tr>
+                  <td><strong>Twitter</strong></td>
+                  <td>It is used to share news and promote the project activites</td>
+                  <td><a target='_blank' href='https://twitter.com/SNSF_4onse'>Click here!</a></td>
+                </tr>
+                <tr>
+                  <td><strong>Linkedin</strong></td>
+                  <td>It is used to share news and promote the project activites</td>
+                  <td><a target='_blank' href='https://www.linkedin.com/in/snsf-4onse-366a45130'>Click here!</a></td>
                 </tr>
               </tbody>
             </Table>
